8e33229d9e74389eb3e61069155be392a1247c02
a-kenji <aks.kenji@protonmail.com>
2022-09-14 14:13:05 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:flake.nix

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  # https://git.gnunet.org/messenger-cli
+  description = "A very basic flake";
+
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
+
+  };
+}
