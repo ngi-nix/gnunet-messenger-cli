f2164db7f21d70144bcec90d7b8556821ae096cf
nuno <info@nunopedrosa.net>
2022-09-14 13:56:54 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:flake.nix

--- flake.nix
+++ flake.nix
@@ -24,6 +24,7 @@
         name = "libgnunetchat";
         src = libgnunetchat;
         nativeBuildInputs = [ pkgs.gnunet pkgs.libsodium pkgs.libgcrypt pkgs.libextractor];
+        outputs = 
       };
     };
 }
